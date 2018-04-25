1f5cce4900f45a2021b84c938ea8a46644f89549
nufail <nufail56@gmail.com>
2018-04-25 22:55:16 +0800

    Use photon colours and theme more elements of UI

--- src/privonyx.js
+++ src/privonyx.js
@@ -1,15 +1,40 @@
 "use strict";
 
+// const ONYX = '#353839';
+const ONYX = '#2F3233';
+const GREY_90 = '#0c0c0d';
+const GREY_70 = '#38383d';
+const GREY_60 = '#4a4a4f';
+const GREY_10_A60 = '#f9f9fa99';
+const GREY_10_A80 = '#f9f9facc';
+const TEAL_70 = '#008ea4';
+
+const YELLOW_90 = '#3e2800';
+const YELLOW_70 = '#a47f00';
+
+const PURPLE_90 = '#25003e';
+const PURPLE_70 = '#6200a4';
+const PURPLE_60 = '#8000d7';
+
 const themes = {
     'onyx': {
         images: {
             headerURL: "",
         },
         colors: {
-            accentcolor: "black",
-            textcolor: "silver",
-            toolbar: "#353839",
-            toolbar_text: "silver"
+            accentcolor: GREY_90,
+            textcolor: GREY_10_A80,
+            toolbar: ONYX,
+            toolbar_text: GREY_10_A80,
+            toolbar_field: GREY_60,
+            toolbar_field_text: GREY_10_A80,
+            icons: GREY_10_A80,
+            icons_attention: TEAL_70,
+            tab_line: TEAL_70,
+            tab_loading: TEAL_70,
+            popup: ONYX,
+            popup_text: GREY_10_A80,
+            popup_border: GREY_90,
         }
     },
     'private': {
@@ -17,10 +42,19 @@ const themes = {
             headerURL: "",
         },
         colors: {
-            accentcolor: "#1A0437",
-            textcolor: "silver",
-            toolbar: "#353839",
-            toolbar_text: "silver"
+            accentcolor: PURPLE_90,
+            textcolor: GREY_10_A80,
+            toolbar: ONYX,
+            toolbar_text: GREY_10_A80,
+            toolbar_field: GREY_60,
+            toolbar_field_text: GREY_10_A80,
+            icons: GREY_10_A80,
+            icons_attention: TEAL_70,
+            tab_line: TEAL_70,
+            tab_loading: TEAL_70,
+            popup: ONYX,
+            popup_text: GREY_10_A80,
+            popup_border: GREY_90,
         }
     },
     'insecure': {
@@ -28,10 +62,39 @@ const themes = {
             headerURL: "",
         },
         colors: {
-            accentcolor: "black",
-            textcolor: "silver",
-            toolbar: "#321E09",
-            toolbar_text: "silver"
+            accentcolor: GREY_90,
+            textcolor: GREY_10_A80,
+            toolbar: YELLOW_90,
+            toolbar_text: GREY_10_A80,
+            toolbar_field: GREY_60,
+            toolbar_field_text: GREY_10_A80,
+            icons: GREY_10_A80,
+            icons_attention: TEAL_70,
+            tab_line: YELLOW_70,
+            tab_loading: YELLOW_70,
+            popup: ONYX,
+            popup_text: GREY_10_A80,
+            popup_border: GREY_90,
+        }
+    },
+    'private-insecure': {
+        images: {
+            headerURL: "",
+        },
+        colors: {
+            accentcolor: PURPLE_90,
+            textcolor: GREY_10_A80,
+            toolbar: YELLOW_90,
+            toolbar_text: GREY_10_A80,
+            toolbar_field: GREY_60,
+            toolbar_field_text: GREY_10_A80,
+            icons: GREY_10_A80,
+            icons_attention: TEAL_70,
+            tab_line: YELLOW_70,
+            tab_loading: YELLOW_70,
+            popup: ONYX,
+            popup_text: GREY_10_A80,
+            popup_border: GREY_90,
         }
     }
 }
@@ -42,17 +105,8 @@ browser.windows.getAll().then(wins => wins.forEach(themeWindow));
 // Theme new windows
 browser.windows.onCreated.addListener(themeWindow);
 
-function themeWindow(window, theme) {
-    // Check if the window is in private browsing
-    if (window.incognito) {
-        browser.theme.update(window.id, themes['private']);
-    }
-
-    // Reset to the default theme otherwise
-    else {
-        //browser.theme.reset(window.id);
-        themeByTab(window.id);
-    }
+function themeWindow(window) {
+    themeByTab(window.id);
 }
 
 function themeByTab(windowId) {
@@ -67,7 +121,11 @@ function themeByTab(windowId) {
 
 function setTheme(tab) {
     if (tab.incognito) {
-        browser.theme.update(tab.windowId, themes['private']);
+        if (tab.url && tab.url.startsWith('http:')) {
+            browser.theme.update(tab.windowId, themes['private-insecure']);
+        } else {
+            browser.theme.update(tab.windowId, themes['private']);
+        }
     } else if (tab.url && tab.url.startsWith('http:')) {
         browser.theme.update(tab.windowId, themes['insecure']);
     } else {
